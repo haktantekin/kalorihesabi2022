f123c8b03962c10e4e423079ab9b129d7b2e8d4f
haktan-tekin <webhaktan@gmail.com>
2022-09-24 22:15:57 +0300

    added 404 page

---
+++ 404.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+<section class="flex-col pt-0 md:pt-7 overflow-hidden">
+    <div class="container">
+      <section class="grid grid-cols-none h-100 w-full gap-0 relative">
+        <div class="flex flex-col">
+              <div class="content-in tracking-wide text-base font-relaway">
+              Biraz zayıflayalım diye sanırım bu sayfayı silmişiz. Bizimle <a href="/iletisim/">iletişim</a>e geçmek ister misin?
+          </div>
+        </div>
+      </section>
+    </div>
+  </section>
+<?php get_footer(); ?>
